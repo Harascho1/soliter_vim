a124c3a8b2194fad516ef8314a5b4efa33850b55
Luka Nikolić <142120455+Harascho1@users.noreply.github.com>
2026-07-24 02:25:58 +0200

    fix(Game-Over): fixed 2 logo printing in game over scene (#25)

--- src/render.c
+++ src/render.c
@@ -4,36 +4,16 @@
 #include "res/res.h"
 #include "res/texture.h"
 
-bool render_background(const GAME* game) {
-  if (game->renderer == NULL) {
-    SDL_Log("game->renderer is NULL\n");
-    return false;
-  }
-
-  bool status = SDL_RenderClear(game->renderer);
-  if (!status) {
-    SDL_Log("SDL_RenderClear failed: %s\n", SDL_GetError());
-    return false;
-  }
-
-  status = SDL_RenderTexture(game->renderer, game->background_texture, NULL, NULL);
+void render_background(const GAME* game) {
+  const bool status =
+    SDL_RenderTexture(game->renderer, game->background_texture, NULL, NULL);
   if (!status) {
     SDL_Log("SDL_RenderTexture failed: %s\n", SDL_GetError());
     push_user_event(SDL_EVENT_QUIT, 0);
-    return false;
   }
-  return status;
 }
 
-bool render_logo(GAME* game) {
-  if (game->menu_texture == NULL) {
-    SDL_Log("menu_texture is NULL\n");
-    game->menu_texture = create_texture_from_image(game->renderer, "assets/logo.png");
-    if (game->menu_texture == NULL) {
-      SDL_Log("menu_texture is NULL\n");
-      return false;
-    }
-  }
+void render_logo(const GAME* game) {
   const SDL_FRect* rect = &(SDL_FRect){
     .x = hero_logo_dimens.margin,
     .y = 0,
@@ -45,7 +25,5 @@ bool render_logo(GAME* game) {
   if (!status) {
     SDL_Log("SDL_RenderTexture failed: %s\n", SDL_GetError());
     push_user_event(SDL_EVENT_QUIT, 0);
-    return false;
   }
-  return status;
 }
